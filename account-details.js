7b15ed23e2bbee602207491cb3f8733efb68acbb
kalaiarasi-entri <kalaiarasientri@gmail.com>
2025-01-28 23:10:46 +0530

    footer added

--- account-details.js
+++ account-details.js
@@ -4,6 +4,7 @@ const termsCheckbox = document.getElementById("terms");
 submitButton.addEventListener("click", (event) => {
      event.preventDefault();
     if (termsCheckbox.checked) {
+        alert("successfully subscribed to newsletter");
         // Navigate to Home page if checkbox is ticked
         window.location.href = 'index.html';  // Redirect to another page
     } else {
